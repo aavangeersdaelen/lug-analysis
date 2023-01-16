24596ade51c9fde59fd363b9d9b1c35349aae7cc
Tony VanGeersdaelen <aavangeersdaelen@crimson.ua.edu>
2023-01-15 20:37:44 -0800

    added lug nomenclature and figures from manual

--- main.py
+++ main.py
@@ -1,6 +1,103 @@
 '''
 Author - Tony VanGeersdaelen
+Date - 01/15/2023
+Source of all equations - AFFDL-TR-69-42, STRESS ANALYSIS MANUAL (AUG-1969)
 
+Lug Analysis Nomenclature
+
+F_bru_L = Lug ulitmate bearing stress
+F_bry_L = Lug yield bearing stress
+F_tux = Cross grain tensile ultimate stress of lug material
+F_tyx = Cross grain tensile yeild stress of lug material
+F_bru = Allowable ultimate bearing stress, MHB 5
+F_bry = Allowalbe yield bearing stress, MHB 5
+F_tu = Ultimate tensile stress
+F_nu_L = Allowable lug net-section tensile ultimate stress
+F_ny_L = Allowable lug net-section tensile yeild stress
+F_bry_B = Allowable bearing yield stress for bushings
+F_cy_B = Bushing compressive yield, stress
+F_bru_B = Allowable bearing ultimate stress for bushings
+F_su_p = Ultimate shear stress of the pin material
+F_tu_p = Pin ultimate tensile stress
+F_tu_T = Allowable ultimate tang stress
+F_br_max_L = Maximum lug bearing stress
+F_br_max_B = Maximum bushing bearing stress
+F_s_max_p = Maximum pin shear stress
+F_b_max_p = Maximum pin bending stress
+
+P_bru_L = Allowable lug ultimate bearing load
+P_nu_L = Allowable lug net-section ultimate load
+P_u_B = Allowable bushing ultimate load
+P_u_L = Allowable design ultimate load
+P_u_L_B = Allowable lug-bushing ultimate load
+P_us_p = Pin ultimate shear load
+P_ub_p = Pin ultimate bending load
+P_ub_p_max = "Balanced design" pin ultimate bending load.
+P_all = Allowable joint ultimate load
+P_T = Lug tang strength
+P_tru_L = Allowable lug transverse ultimate load
+P_tru_B = Allowable bushing transverse ultimate load
+
+K_n = Net-tension stress coefficient
+K_b_p = Plastic bending coefficient for pin
+K_b_T = Plastic bending coefficient for tang
+K_br_T = Plastic bearing coefficient for lug
+K_b_L = Plastic bending coefficient for lug
+K_tru = Transverse ultimate load coefficient
+K_try = Transverse yield. load coefficient
+
+M_max_p = Maximum pin bending moment
+M_u_p = Ultimate pin failing moment
+
+A = Area, in^2
+a = Distance from edge of hole to edge of lug, inches
+B = Ductility factor for lugs with less than 5% elongation
+b = Effective bearing width, inches
+D = Hole diameter of pin diameter, inches
+E = Modulus of elasticity, psi
+e = Edge distance, inches
+f = Stress, psi
+f_a = Cyclic stress amplitude on net section of given lug, lbs/in^2
+f_n = Mean cyclic stress on net section of given lug, lbs/in^22
+f_max = Maximum cyclic stress on net section of given lug, lbs/in^2
+f_min = Minimum cyclic stress on net section of given lug, lbs/in^2
+g = Gap between lugs, inches
+hl.. h4 = Edge distances in transversely loaded lug, inches
+h_av = Effective edge distance in transversely loaded lug
+K = Allowable stress (or load) coefficient
+k_1, k_2, k_3 = Fatigue parameters
+M = Bending moment, in-lbs.
+N = Fatigue life, number of cycles
+P = Load, lbs.
+R = Stress ratio, fUhI/fUI
+t_B = Bushing wall thickness, inches
+t = Lug thickness, inches
+w = Lug width, inches
+O = Angle of load to axial direction, degrees
+e = Strain, inches/inch
+P = Density, lbs/in^3
+
+Subscripts
+al = Allowable
+opt = Optimum
+ax = Axial
+p = Pin
+B = Bushing
+s = Shear
+b = Bending
+T = Tang
+br = Bearing
+t = Tensile
+C = Compression
+tr = Transverse
+L = Lug
+U = Ultimate
+max = Maximum
+x = Cross grain
+n = Net tensile
+y = Yield
+0 = Oblique
+1, 2 = Female and male lugs
 '''
 
 if __name__ == "__main__":
